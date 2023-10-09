d4ed947274e19265dd07456430c82ffa2b4678fd
jurwon <susorrow@naver.com>
2023-10-09 19:58:42 +0900

    1009 restaurant entity 수정

--- src/main/java/com/matzip/entity/Restaurant.java
+++ src/main/java/com/matzip/entity/Restaurant.java
@@ -15,17 +15,40 @@ public class Restaurant {
 
     @Id
     @Column(name="res_id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long res_id;       //식당 번호
+    private Long res_id;       //식당 id
 
     @Column(nullable = false)
     private String res_name;//식당 이름
 
     @Column(nullable = false)
+    private String res_district;//구군
+
+    @Column(nullable = false)
+    private String res_latitude;//위도
+
+    @Column(nullable = false)
+    private String res_hardness;//경도
+
+    @Column(nullable = false)
     private String res_address;//식당 주소
 
+    @Column(nullable = false)
+    private String res_phone;//연락처
+
+    @Column(nullable = false)
+    private String operate_time;//운영 및 시간
+
+    @Column(nullable = false)
+    private String res_menu;//대표 메뉴
+
     @Column
-    private String res_image; //식당 사진
+    private String res_image; //식당 이미지 url
+
+    @Column
+    private String res_thumbnail; //썸네일 이미지 url
+
+    @Column
+    private String res_intro; //가게 소개
 
     /*@Column(nullable = false)
     private String avg_score; //평균 평점*/
