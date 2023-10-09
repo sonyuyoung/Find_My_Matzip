cd6e8d176027e47b996520fec9f330dde219cb76
jurwon <susorrow@naver.com>
2023-10-09 20:46:03 +0900

    1009 1. board, board_img, restaurant 테이블 추가 2. boardDto, BoardFormDto, BoardImgDto 추가 3. profileForm 추가 -> 메뉴에 연결

---
+++ src/main/java/com/matzip/entity/Restaurant.java
@@ -0,0 +1,59 @@
+package com.matzip.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="restaurant")
+@Getter
+@Setter
+@ToString
+public class Restaurant {
+
+    @Id
+    @Column(name="res_id", nullable = false)
+    private Long res_id;       //식당 id
+
+    @Column(nullable = false)
+    private String res_name;//식당 이름
+
+    @Column(nullable = false)
+    private String res_district;//구군
+
+    @Column(nullable = false)
+    private String res_latitude;//위도
+
+    @Column(nullable = false)
+    private String res_hardness;//경도
+
+    @Column(nullable = false)
+    private String res_address;//식당 주소
+
+    @Column(nullable = false)
+    private String res_phone;//연락처
+
+    @Column(nullable = false)
+    private String operate_time;//운영 및 시간
+
+    @Column(nullable = false)
+    private String res_menu;//대표 메뉴
+
+    @Column
+    private String res_image; //식당 이미지 url
+
+    @Column
+    private String res_thumbnail; //썸네일 이미지 url
+
+    @Column
+    private String res_intro; //가게 소개
+
+    /*@Column(nullable = false)
+    private String avg_score; //평균 평점*/
+
+}
