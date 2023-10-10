8c78808220298febe4be6305d223f1f3f485292b
jurwon <susorrow@naver.com>
2023-10-10 12:03:30 +0900

    1010 중간저장

--- src/main/java/com/matzip/controller/BoardController.java
+++ src/main/java/com/matzip/controller/BoardController.java
@@ -1,12 +1,16 @@
 package com.matzip.controller;
 
+import com.matzip.entity.Users;
 import com.matzip.repository.BoardRepository;
+import com.matzip.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 
 @Controller
@@ -15,12 +19,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BoardController {
 
     private final BoardRepository boardRepository;
+    private final UsersRepository usersRepository;
 
 
     //게시글 작성 폼 띄우기
     @GetMapping(value = "/boardForm")
-    public String boardForm(){
-
+    public String boardForm(Principal principal,Model model){
+        String userid = principal.getName(); //회원 id 받기
+        Users users = usersRepository.findByUserid(userid);
+        model.addAttribute("users",users);
         return "/board/boardForm";
     }
 
