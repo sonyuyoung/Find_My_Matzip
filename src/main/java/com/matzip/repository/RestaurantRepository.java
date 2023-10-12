23b405fdc63565533c4079c4454a311d5c358eba
옥 <eastok95@gmail.com>
2023-10-12 10:16:36 +0900

    불필요구문주석

--- src/main/java/com/matzip/repository/RestaurantRepository.java
+++ src/main/java/com/matzip/repository/RestaurantRepository.java
@@ -3,10 +3,8 @@ package com.matzip.repository;
 import com.matzip.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface RestaurantRepository extends JpaRepository<Restaurant, String>{
     //위도와 경도로 조회
-    List<Restaurant> findByres_latAndres_lng(String res_lat, String res_lng);
+   // List<Restaurant> findByres_latAndres_lng(String res_lat, String res_lng);
 
 }
