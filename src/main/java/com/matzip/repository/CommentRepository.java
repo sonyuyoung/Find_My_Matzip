acf6f9ab5b156ec2a1fb72303e6f742503fa254a
02 <sonyuyoung@naver.com>
2023-12-18 21:47:25 +0900

    대댓글 , drop table comment;하고 다시실행

--- src/main/java/com/matzip/repository/CommentRepository.java
+++ src/main/java/com/matzip/repository/CommentRepository.java
@@ -11,17 +11,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-//    Optional<Comment> findById(Long commentId);
-
-    // select * from comment_table where board_id=? order by id desc;
-//    List<Comment> findAllByBoardOrderByIdDesc(Board board);
 
     Optional<Comment> findByCommentId(Long commentId);
 
     List<Comment> findByBoardIdOrderByCreationDateDesc(Long boardId);
 
     Page<Comment> findPageByBoardIdOrderByCreationDateDesc(Long boardId, Pageable pageable);
-
 
     List<Comment> findByBoardIdAndParent_CommentIdOrderByCommentIdDesc(Long boardId, Long parentId);
 
